38aaab0e060bf72d6333226a630802698910a584
John Rey <adioskid@mail.ru>
2022-11-24 21:35:49 +0300

    FinalPuzzles

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -1,7 +1,13 @@
 public class FirstTask {
 
-    
-    public static void main(String[] args) {
+    static int getWholeRandom(int a, int b) {
+        java.util.Random rand = new java.util.Random();
+        return rand.nextInt(b - a + 1) + a;
+    }
 
+
+    public static void main(String[] args) {
+        int x = getWholeRandom(5, 10);
+        System.out.println(x);
     }
 }
