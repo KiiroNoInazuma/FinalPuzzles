d4fe0cffacf0fff65778a6ad85d61e0b353330f7
John Rey <adioskid@mail.ru>
2022-11-24 22:18:17 +0300

    FinalPuzzles

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -11,7 +11,7 @@ public class Palindrome {
 
 
     public static void main(String[] args) {
-        System.out.println(palindrome("Комок"));
+        System.out.println(palindrome("ротор"));
     }
 }
 
